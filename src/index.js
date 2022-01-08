a6dbcc33e85276caa89dd1cfd988aaa0c633e3f3
Ganesh Pawar <ganesh.pawar@vshift.com>
2022-01-08 19:45:35 +0530

    coinbase learn and detail template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { CmsCore } from "crownpeak-dxm-react-sdk";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Routing from "./routing";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+console.log("env: ", process.env.REACT_APP_CMS_STATIC_CONTENT_LOCATION);
+
+CmsCore.init(
+  process.env.REACT_APP_CMS_STATIC_CONTENT_LOCATION,
+  process.env.REACT_APP_CMS_DYNAMIC_CONTENT_LOCATION
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+Routing.processRoutes(
+  process.env.REACT_APP_CMS_STATIC_CONTENT_LOCATION + "/routes.json"
+);
